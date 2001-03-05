b0fd725e972d5e964d85d04955a080dcaf6304cf
JP Rosevear <jpr@ximian.com>
2001-03-05 05:18:04 +0000

    don't just set basic properties if there are keywords
    2001-03-05  JP Rosevear  <jpr@ximian.com>
    
    	* scintilla-control/scintilla-highlight.c
    	(set_language_properties): don't just set basic properties if
    	there are keywords

--- scintilla-control/scintilla-highlight.c
+++ scintilla-control/scintilla-highlight.c
@@ -17,19 +17,25 @@ struct ExtensionLexer {
  * of some sort.  But I'm too lazy at the moment, so it is all hard-coded 
  * in. */
 
-#define idl_keywords "switch union raises interface module in out void long boolean short string struct enum"
+#define idl_keywords "allocate appobject async auto_handle bindable boolean broadcast byte byte_count call_as callback char coclass code comm_status const context_handle control cpp_quote decode default defaultbind defaultvalue dispinterface displaybind dllname double dual enable_allocate encode endpoint entry enum error_status_t explicit_handle fault_status first_is float handle_t heap helpcontext helpfile helpstring hidden hyper id idempotent ignore iid_is immediatebind implicit_handle import importlib in include in_line int __int64 interface last_is lcid length_is library licensed local long max_is maybe message midl_user_allocate midl_user_free min_is module ms_union ncacn_at_dsp ncacn_dnet_nsp ncacn_http ncacn_ip_tcp ncacn_nb_ipx ncacn_nb_nb ncacn_nb_tcp ncacn_np ncacn_spx ncacn_vns_spp ncadg_ip_udp ncadg_ipx ncadg_mq ncalrpc nocode nonextensible notify object odl oleautomation optimize optional out out_of_line pipe pointer_default propget propput propputref public ptr readonly ref represent_as requestedit restricted retval shape short signed size_is small source string struct switch switch_is switch_type transmit_as typedef"
+
 #define c_keywords "asm auto break case catch char class const const_cast continue default delete do double dynamic_cast else enum explicit export extern false float for friend goto if inline int long mutable register return short signed sizeof static struct switch typedef union unsigned void volatile wchar_t while"
+
 #define cpp_keywords "asm auto bool break case catch char class const const_cast continue default delete do double dynamic_cast else enum explicit export extern false float for friend goto if inline int long mutable namespace new operator private protected public register reinterpret_cast return short signed sizeof static static_cast struct switch template this throw true try typedef typeid typename union unsigned using virtual void volatile wchar_t while"
+
+#define html_keywords "abbr accept-charset accept accesskey action align alink alt archive axis background bgcolor border cellpadding cellspacing char charoff charset checked cite class classid clear codebase codetype color cols colspan compact content coords data datafld dataformatas datapagesize datasrc datetime declare defer dir disabled enctype event face for frame frameborder headers height href hreflang hspace http-equiv id ismap label lang language leftmargin link longdesc marginwidth marginheight maxlength media method multiple name nohref noresize noshade nowrap object onblur onchange onclick ondblclick onfocus onkeydown onkeypress onkeyup onload onmousedown onmousemove onmouseover onmouseout onmouseup onreset onselect onsubmit onunload profile prompt readonly rel rev rows rowspan rules scheme scope selected shape size span src standby start style summary tabindex target text title topmargin type usemap valign value valuetype version vlink vspace width text password checkbox radio submit reset file hidden image"
+
+#define python_keywords "and assert break class continue def del elif else except exec finally for from global if import in is lambda None not or pass print raise return try while"
 
 struct ExtensionLexer lexer_map[] = {
     { "text/x-idl", SCLEX_CPP, idl_keywords },
     { "text/x-c", SCLEX_CPP, c_keywords },
     { "text/x-c++", SCLEX_CPP, cpp_keywords },
-    { "text/html", SCLEX_HTML, NULL },
+    { "text/html", SCLEX_HTML, html_keywords },
     { "text/xml", SCLEX_XML, NULL },
     { "text/x-perl", SCLEX_PERL, NULL },
     { "text/x-sql", SCLEX_SQL, NULL },
-    { "text/x-python", SCLEX_PYTHON, NULL },
+    { "text/x-python", SCLEX_PYTHON, python_keywords },
     { "text/x-makefile", SCLEX_MAKEFILE, NULL },
     { NULL, 0 }
 };
@@ -37,30 +43,30 @@ struct ExtensionLexer lexer_map[] = {
 void 
 set_language_properties (ScintillaObject *sci, const char *mime_type)
 {
-    struct ExtensionLexer *lexer;
-    long lex = SCLEX_NULL;
-    char *keywords = NULL;
-    char *font = "courier";
+	struct ExtensionLexer *lexer;
+	long lex = SCLEX_NULL;
+	char *keywords = NULL;
+	char *font = "courier";
 
-    for (lexer = lexer_map; lexer->mime_type; lexer++) {
-      if (!strcmp (lexer->mime_type, mime_type)) {
-	lex = lexer->lexer;
-	keywords = lexer->keywords;
-	break;
-      }
-    }
+	for (lexer = lexer_map; lexer->mime_type; lexer++) {
+		if (!strcmp (lexer->mime_type, mime_type)) {
+			lex = lexer->lexer;
+			keywords = lexer->keywords;
+			break;
+		}
+	}
 
-    if ((lex == SCLEX_HTML)||(lex == SCLEX_XML)) 
-	scintilla_send_message (sci, SCI_SETSTYLEBITS, 7, 0);
-    else
-	scintilla_send_message (sci, SCI_SETSTYLEBITS, 5, 0);
-    scintilla_send_message (sci, SCI_SETLEXER, lex, 0);
+	if ((lex == SCLEX_HTML)||(lex == SCLEX_XML)) 
+		scintilla_send_message (sci, SCI_SETSTYLEBITS, 7, 0);
+	else
+		scintilla_send_message (sci, SCI_SETSTYLEBITS, 5, 0);
+	scintilla_send_message (sci, SCI_SETLEXER, lex, 0);
 
-    scintilla_send_message (sci, SCI_STYLERESETDEFAULT, 0, 0);
-    scintilla_send_message (sci, SCI_STYLECLEARALL, 0, 0);
+	scintilla_send_message (sci, SCI_STYLERESETDEFAULT, 0, 0);
+	scintilla_send_message (sci, SCI_STYLECLEARALL, 0, 0);
 
-    if (keywords) {
-	scintilla_send_message (sci, SCI_SETKEYWORDS, 0, (long)keywords);
+	if (keywords)
+		scintilla_send_message (sci, SCI_SETKEYWORDS, 0, (long)keywords);
 	
 	/* Whitespace */ 
 	scintilla_send_message (sci, SCI_STYLESETFONT,0,(long)font);
@@ -108,8 +114,5 @@ set_language_properties (ScintillaObject *sci, const char *mime_type)
 	/* Identifiers */
 	scintilla_send_message (sci, SCI_STYLESETFONT,11,(long)font);
 
-    }
-    scintilla_send_message (sci, SCI_COLOURISE, 0, -1);
-
-
+	scintilla_send_message (sci, SCI_COLOURISE, 0, -1);
 }
