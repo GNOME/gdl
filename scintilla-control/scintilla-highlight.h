f0edaaf23cf48959004d249a4172b4dc87adff2e
JP Rosevear <jpr@ximian.com>
2002-03-18 00:22:10 +0000

    remove scintilla-control dir reference
    2002-03-17  JP Rosevear  <jpr@ximian.com>
    
    	* Makefile.am: remove scintilla-control dir reference
    
    	* Remove scintilla stuff for good

--- scintilla-control/scintilla-highlight.h
+++
@@ -1,8 +0,0 @@
-#ifndef __SCINTILLA_HIGHLIGHT_H__
-#define __SCINTILLA_HIGHLIGHT_H__
-
-#include "scintilla/ScintillaWidget.h"
-
-void set_language_properties (ScintillaObject *sci, const char *mime_type);
-
-#endif
