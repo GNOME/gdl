4208d30c5554d4d08e91f73dee1246520946606c
Jeroen Zwartepoorte <jeroen@xs4all.nl>
2002-04-20 20:52:46 +0000

    Added James Willcox <jwillcox@cs.indiana.edu> (original author of
    2002-04-20  Jeroen Zwartepoorte  <jeroen@xs4all.nl>
    
    	* gdl/gdl-recent.c: Added James Willcox <jwillcox@cs.indiana.edu>
    	(original author of gnome-recent on which gedit-recent is based, on
    	which gdl-recent is based) to copyright list.
    	* gdl/gdl-recent.h: Idem.

--- gdl/gdl-recent.c
+++ gdl/gdl-recent.c
@@ -2,6 +2,7 @@
  * This file is part of the GNOME Devtools Libraries.
  *
  * Copyright (C) 2002 Jeroen Zwartepoorte
+ *                    James Willcox <jwillcox@cs.indiana.edu>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
